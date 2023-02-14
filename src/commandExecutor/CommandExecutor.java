f185c9487afb70c400f9d98d2c9d9a8846cad9e2
MikhailKadilov <kadilov2004@mail.ru>
2023-02-14 15:49:30 +0300

    Добавка

--- src/commandExecutor/CommandExecutor.java
+++ src/commandExecutor/CommandExecutor.java
@@ -1,5 +1,8 @@
 package commandExecutor;
 
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CommandExecutor {
@@ -13,10 +16,89 @@ public class CommandExecutor {
         while (execution) {
             String line = console.nextLine();
             execute(line);
+
         }
     }
 
     public void execute(String line) {
+        if (line.equals("help")) {
+            help();
+        }
+        else if (line.equals("info")) {
+            ;
+        }
+        else if (line.equals("show")){
+            ;
+        }
+        else if (line.indexOf("insert null ") == 0){
+            ;
+        }
+        else if (line.indexOf("update id ") == 0){
+            ;
+        }
+        else if (line.indexOf("remove_key ") == 0){
+            ;
+        }
+        else if (line.equals("clear")){
+            ;
+        }
+        else if (line.equals("save")){
+            ;
+        }
+        else if (line.indexOf("execute_script ") == 0){
+            ;
+        }
+        else if (line.equals("exit")){
+            ;
+        }
+        else if (line.indexOf("remove_lower ") == 0){
+            ;
+        }
+        else if (line.equals("history")){
+            ;
+        }
+        else if (line.indexOf("replace_if_greater ") == 0){
+            ;
+        }
+        else if (line.indexOf("count_by_event ") == 0){
+            ;
+        }
+        else if (line.indexOf("filter_contains_name ") == 0){
+            ;
+        }
+        else if (line.equals("print_field_descending_event")){
+            ;
+        }
+    }
+
+    public void help() {
+        HashMap<Integer, String> passportsAndNames = new HashMap<>();
+
+        HashMap<String, String> commandMap = new HashMap<String, String>() {{
+            put("help", ": вывести справку по доступным командам");
+            put("info", ": вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)");
+            put("show", ": вывести в стандартный поток вывода все элементы коллекции в строковом представлении");
+            put("insert null {element}", ": добавить новый элемент с заданным ключом");
+            put("update id {element}", ": обновить значение элемента коллекции, id которого равен заданному");
+            put("remove_key null", ": удалить элемент из коллекции по его ключу");
+            put("clear",  ": очистить коллекцию");
+            put("save", ": сохранить коллекцию в файл");
+            put("execute_script file_name", ": считать и исполнить скрипт из указанного файла. В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.");
+            put("exit", ": завершить программу (без сохранения в файл)");
+            put("remove_lower {element}", ": удалить из коллекции все элементы, меньшие, чем заданный");
+            put("history", ": вывести последние 7 команд (без их аргументов)");
+            put("replace_if_greater null {element}", ": заменить значение по ключу, если новое значение больше старого");
+            put("count_by_event event", ": вывести количество элементов, значение поля event которых равно заданному");
+            put("filter_contains_name name", ": вывести элементы, значение поля name которых содержит заданную подстроку");
+            put("print_field_descending_event", ": вывести значения поля event всех элементов в порядке убывания");
+        }};
+
+        for (String name: commandMap.keySet()) {
+            String key = name;
+            String value = commandMap.get(name);
+            System.out.println(key + " " + value);
+        }
+
 
     }
 }
